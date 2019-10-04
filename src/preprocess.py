81bce37962e15f91c0fd53182eb10a2637ee0ae7
nur-ag <do.not.bother@me.com>
2019-10-04 22:02:35 +0200

    Initial dummy commit with some groundwork plan that will eventually go wrong -- I see you, voracious reader of commit histories: from the commit you read first, at the very top, you have seen the madness that sprung from this initial tale to be told...

---
+++ src/preprocess.py
@@ -0,0 +1,5 @@
+print('This file should preprocess the dataset into a vector representation for simpler loading.')
+print('It all should be dumped in ../preprocessed/')
+print('Ideally we will end up with beautiful NumPy matrices to load the data easily')
+
+
---
+++ src/run.py
@@ -0,0 +1,3 @@
+print('This script is the main point of entry.')
+print('It should let you run all the cascading steps to replicate the paper, altogether or alone')
+print('It also takes care of distributing what can be distributed!')
