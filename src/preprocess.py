fb8dec751e45dbf6c240224d0675221d8ba09b8e
Nur Álvarez González <nurloco@hotmail.com>
2019-11-24 21:28:11 +0100

    That's a first working version, alright

--- src/preprocess.py
+++ src/preprocess.py
@@ -1,5 +1,46 @@
-print('This file should preprocess the dataset into a vector representation for simpler loading.')
-print('It all should be dumped in ../preprocessed/')
-print('Ideally we will end up with beautiful NumPy matrices to load the data easily')
+import numpy as np
+import pandas as pd
 
+emotions = pd.read_csv('../data/emotions.csv')
+emotion_categories = pd.read_csv('../data/emotion_categories.csv')
+graph = pd.read_csv('../data/vent.edgelist', names=['source_user_id', 'dest_user_id'], sep=' ')
+vents = pd.read_csv('../data/vents.csv')
 
+# Compute emotion category indices
+emotion_categories.columns = ['emotion_category_id', 'emotion_category_name']
+all_emotion_categories = sorted(emotion_categories.emotion_category_id.unique())
+emotion_category_indices = {c: i for i, c in enumerate(all_emotion_categories)}
+emotion_categories['emotion_category_index'] = [emotion_category_indices[c] for c in emotion_categories['emotion_category_id'].tolist()]
+
+# Compute emotion indices and build the whole emotion table
+emotions.columns = [u'emotion_id', u'emotion_category_id', u'name', u'enabled']
+all_emotions = sorted(emotions.emotion_id.unique())
+emotion_indices = {e: i for i, e in enumerate(all_emotions)}
+emotions['emotion_index'] = [emotion_indices[c] for c in emotions['emotion_id'].tolist()]
+emotions_full = emotions.merge(emotion_categories, on='emotion_category_id')
+emotions_full.columns = emotions_full.columns.astype(str)
+emotions_full.to_parquet('../preprocessed/emotions.parquet')
+
+# Find all the users and replace them in the graph
+all_users = sorted(set(graph.source_user_id.unique().tolist() + graph.dest_user_id.unique().tolist()))
+users = {v: i for i, v in enumerate(all_users)}
+users_df = pd.DataFrame([(i, v) for (v, i) in users.items()], columns=['user_index', 'user_id'])
+users_df.to_parquet('../preprocessed/users.parquet')
+graph['source_user_index'] = [users[u] for u in graph.source_user_id.tolist()]
+graph['dest_user_index'] = [users[u] for u in graph.dest_user_id.tolist()]
+del graph['source_user_id']
+del graph['dest_user_id']
+graph.to_csv('../preprocessed/vent-user-indices.edgelist', sep='\t', index=False)
+
+# Compute the complete vent dataset, preprocessing fields
+vents_with_users = vents.merge(users_df, on='user_id')
+vents_with_users['created_at'] = pd.to_datetime(vents_with_users['created_at'])
+del vents_with_users['user_id']
+vents_with_users['emotion_index'] = [emotion_indices[e] for e in vents_with_users['emotion_id'].tolist()]
+del vents_with_users['emotion_id']
+
+# Add the emotion category indices directly, so the dataset contains everything at a glance
+emotion_cats = emotions_full[['emotion_index', 'emotion_category_index']]
+vents_with_users = vents_with_users.merge(emotion_cats, on='emotion_index')
+vents_with_users.columns = vents_with_users.columns.astype(str)
+vents_with_users.to_parquet('../preprocessed/vent.parquet')
--- src/run.py
+++ src/run.py
@@ -1,3 +1,78 @@
-print('This script is the main point of entry.')
-print('It should let you run all the cascading steps to replicate the paper, altogether or alone')
-print('It also takes care of distributing what can be distributed!')
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.linear_model import SGDClassifier
+from sklearn.metrics import precision_recall_fscore_support
+
+
+NUM_WORDS = 500000
+BATCH_SIZE = 100000
+TARGET_COLUMN = 'emotion_category_index'
+
+def fit_partial(X, y, clf, batch_size):
+    num_instances = X.shape[0]
+    total_batches = (num_instances + batch_size - 1) // batch_size
+    classes = y.unique()
+    for i in range(total_batches):
+        batch_start = i * batch_size
+        batch_end = (i + 1) * batch_size
+        batch_end = min(batch_end, num_instances)
+        X_batch = X[batch_start:batch_end]
+        y_batch = y[batch_start:batch_end]
+        clf.partial_fit(X_batch, y_batch, classes=classes)
+    return clf
+
+
+def predict_partial(X, clf, batch_size):
+    num_instances = X.shape[0]
+    total_batches = (num_instances + batch_size - 1) // batch_size
+    results = []
+    for i in range(total_batches):
+        batch_start = i * batch_size
+        batch_end = (i + 1) * batch_size
+        batch_end = min(batch_end, num_instances)
+        X_batch = X[batch_start:batch_end]
+        results.extend(clf.predict(X_batch).flatten())
+    return np.asarray(results).reshape(-1, 1)
+
+
+vent = pd.read_parquet('../preprocessed/vent.parquet')
+frequent_values = vent[TARGET_COLUMN].value_counts()[:9].index
+vent_frequent = vent #vent[vent[TARGET_COLUMN].isin(frequent_values)]
+task_data = vent_frequent[['text', TARGET_COLUMN]].dropna()
+print('Data loading ready...')
+
+rest, test = train_test_split(task_data, test_size=0.2)
+train, valid = train_test_split(rest, test_size=0.25)
+del vent, rest
+print('Splits are ready...')
+
+vec = TfidfVectorizer(stop_words='english', max_features=NUM_WORDS)
+X_train = vec.fit_transform(train.text)
+X_test = vec.transform(test.text)
+print('TF-IDF is ready...')
+
+clf = MultinomialNB()
+clf = fit_partial(X_train, train[TARGET_COLUMN], clf, BATCH_SIZE)
+print('Training is done...')
+
+y_p_train = predict_partial(X_train, clf, BATCH_SIZE)
+y_p_test = predict_partial(X_test, clf, BATCH_SIZE)
+print('Predictions are done...')
+
+print('Training data performance: ')
+stats = precision_recall_fscore_support(train[TARGET_COLUMN].to_numpy(), y_p_train)
+for (p, r, f, s) in zip(*stats):
+    print('Precision: {}\tRecall: {}\tF1-score: {}\tSupport: {}'.format(p, r, f, s))
+print('Mean macro F1-score: {}'.format(np.mean(stats[2])))
+print('Mean micro F1-score: {}'.format(np.sum(stats[2] * stats[3]) / stats[3].sum()))
+
+print('Test data performance: ')
+stats = precision_recall_fscore_support(test[TARGET_COLUMN].to_numpy(), y_p_test)
+for (p, r, f, s) in zip(*stats):
+    print('Precision: {}\tRecall: {}\tF1-score: {}\tSupport: {}'.format(p, r, f, s))
+print('Mean macro F1-score: {}'.format(np.mean(stats[2])))
+print('Mean micro F1-score: {}'.format(np.sum(stats[2] * stats[3]) / stats[3].sum()))
+    
