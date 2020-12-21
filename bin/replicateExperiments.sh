8a6a1ef7ef857bce9ff86a33e74acf49e18dc9ba
Nur Álvarez González <nurloco@hotmail.com>
2020-12-21 22:49:20 +0100

    Add stationarity replication experiments

--- bin/replicateExperiments.sh
+++ bin/replicateExperiments.sh
@@ -1,4 +1,16 @@
+# Benchmark experiments
 sbatch --export=CONFIG_PATH='configs/GoEmotions/replica/classic/' ./bin/runWithLimit.sh
 sbatch --export=CONFIG_PATH='configs/GoEmotions/replica/neural/' ./bin/runWithLimitGPU.sh
 sbatch --export=CONFIG_PATH='configs/Vent/replica/classic/' ./bin/runWithLimit.sh
 sbatch --export=CONFIG_PATH='configs/Vent/replica/neural/' ./bin/runWithLimitGPU.sh
+
+# Non-stationarity experiments
+sbatch --export=CONFIG_PATH='configs/Vent/replica-full/classic/' ./bin/runWithLimit.sh
+sbatch --export=CONFIG_PATH='configs/Vent/replica-full/neural/' ./bin/runWithLimitGPU.sh
+sbatch --export=CONFIG_PATH='configs/Vent/replica-random/classic/' ./bin/runWithLimit.sh
+sbatch --export=CONFIG_PATH='configs/Vent/replica-random/neural/' ./bin/runWithLimitGPU.sh
+sbatch --export=CONFIG_PATH='configs/Vent/replica-full-random/classic/' ./bin/runWithLimit.sh
+sbatch --export=CONFIG_PATH='configs/Vent/replica-full-random/neural/' ./bin/runWithLimitGPU.sh
+
+# Transfer learning experiments
+# ToDo
